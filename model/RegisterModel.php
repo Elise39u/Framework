b58d0d5d02a245b15c410e805f1904fd45d9679d
Justin van de laar <justin555@live.nl>
2017-03-20 17:15:47 +0100

    Completed the register part

--- model/RegisterModel.php
+++ model/RegisterModel.php
@@ -9,19 +9,22 @@ function Checker($firstname, $prefire, $lastname, $email, $username, $password) 
      	':username' => $username
      	));
 
-     $pdo = PDO::FETCH_BOUND;
-     $resultaat = $querySelect->$pdo;
-     var_dump($resultaat);
-     if ($querySelect < 0) {
+     // setFetch($querySelect);
+     $count = $querySelect->rowCount();
+     var_dump($count);
+     if ($count < 0) {
      	echo "Nothing has been found";
-     } elseif ($querySelect == 1) {
+     } elseif ($count == 1) {
 
      } else {
      	Register($firstname, $prefire, $lastname, $email, $username, $password);
      }
+
+   	$db = null;
 }
 
 function Register($firstname, $prefire, $lastname, $email, $username, $password) { 
+	$db = openDatabaseConnection();
      $sql = "INSERT INTO user(firstname, prefire, lastname, email, username, password) VALUES(:firstname, :prefire, :lastname, :email, :username, :password)";
      $query = $db->prepare($sql);
      $query->execute(array(
